880ad7b536ce7868031cbc923880e6e2d4143678
Krystian Fryca <kp15731@gmail.com>
2023-03-27 14:57:24 +0200

    convert a string to an array Kata

---
+++ C++/convertAStringToAnArray/convertAStringToAnArray/convertAStringToAnArray.cpp
@@ -0,0 +1,45 @@
+/*
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<std::string> string_to_array(const std::string& s) {
+    if (s.empty())return { "" };
+
+    std::vector<std::string> vec;
+    std::string tmp = "";
+    for (char c : s) {
+        if (c == ' ') {
+            vec.push_back(tmp);
+            tmp = "";
+        }
+        else tmp += c;
+    }
+    if (!tmp.empty()) vec.push_back(tmp);
+    return vec;
+}
+template<typename T>
+bool equal(T a, T b) {
+    return a == b;
+}
+
+int main()
+{
+    std::cout << std::boolalpha;
+
+    std::cout << equal(string_to_array("some value"), { "some", "value" }) << "\n";
+    std::cout << equal(string_to_array("Robin Singh"), { "Robin", "Singh" }) << "\n";
+    std::cout << equal(string_to_array("Codewars"), { "Codewars" }) << "\n";
+    std::cout << equal(string_to_array("I love arrays"), { "I", "love", "arrays" }) << "\n";
+    std::cout << equal(string_to_array("1 2 3"), { "1", "2", "3" }) << "\n";
+    std::cout << equal(string_to_array(""), { "" }) << "\n";
+
+    return 0;
+}
